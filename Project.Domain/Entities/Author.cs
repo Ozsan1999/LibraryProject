6e52967f595e7cbab2395e50a87ffc020a5ab799
Cristian Sanjuan <cristiansanjuanozuna@hotmail.com>
2023-08-18 17:17:18 -0500

    Entidades y propiedades

---
+++ Project.Domain/Entities/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string CityOfOrigin { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Project.Domain/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public int NumberOfPages { get; set; }
+
+        // Navigation property
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
